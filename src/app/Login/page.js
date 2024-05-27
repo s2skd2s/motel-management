e02b0b96fcdc88d1bb6732f54ded9571c9aa36c1
Sam Khanh Duy <samkhanhduy2002@gmail.com>
2024-05-27 17:22:24 +0700

    feat: add login and logout

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -1,15 +1,29 @@
-import styles from './Login.module.css'
+'use client'
+
+import styles from './Login.module.css';
+import { useEffect, useState } from 'react';
 export default function Login(){
+    // Giả sử đây là hàm xử lý đăng nhập
+    function handleLogin() {
+        const token = 'token';
+         // Token received from the server after successful login
+        if (typeof window !== "undefined") {
+          localStorage.setItem('authToken', token);
+        }
+    }
+
     return(
     <main className={styles.main}>
         <div className={styles.login_container}>
             <h1 className={styles.login_title}>Đăng nhập</h1>
             <form>
                 <label className={styles.login_label}>SỐ ĐIỆN THOẠI</label>
-                <input type='text' className={styles.login_input} required  ></input>
+                <input type='text' id='login_input' className={styles.login_input} required  ></input>
                 <label className={styles.login_label}>MẬT KHẨU</label>
                 <input type='password' className={styles.login_input} required ></input>
-                <button className={styles.login_btn}>Đăng nhập</button>
+                <a className={styles.login_btn} onClick={() => handleLogin()} href='/'> 
+                        Đăng nhập    
+                </a>
             </form>
             <div className={styles.others_link_container}>
                 <a href='/' className={styles.others_link}>Bạn quên mật khẩu?</a>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,24 @@
+'use client'
+
 import styles from './Header.module.css';
+import React, {useState, useEffect} from 'react';
 export default function Header(){
+    // utils/auth.js
+    var isLoggedIn = function () {
+        const token = localStorage.getItem('authToken');
+        return token !== null;
+    }
+
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    useEffect(() => {
+        setLoggedIn(isLoggedIn());
+    }, []);
+
+    var handleLogout = function () {
+        localStorage.removeItem('authToken');
+    }
+
     return(
         <div className={styles.header_container}>
             <div className={styles.left_header}>
@@ -7,11 +26,21 @@ export default function Header(){
                 </a>
             </div>
             <div className={styles.right_header}>
-                <a className={styles.header_btn} href="/Login">
+                <div className={`${styles.logout_btn} ${loggedIn ? styles.isLoggedIn : ""}`} >
+                    <img className={styles.user_image} src='/images/icons/default-user-icon.png'></img>
+                    <div style={{borderRight: "1px solid black"}}>
+                        <p className={styles.username}>Nguyễn Văn A</p>
+                    </div>
+                    <a className={styles.header_btn} href='/Login' onClick={()=> handleLogout()}> 
+                        <img className={styles.logo} src='/images/icons/room-type-icon.png'></img>
+                        Đăng xuất
+                    </a>
+                </div>
+                <a className={`${styles.header_btn} ${loggedIn ? styles.isLoggedIn : ""}`} href="/Login">
                     <img className={styles.logo} src='/images/layout/header/login-logo.jpg'></img>
                     Đăng nhập
                 </a>
-                <a className={styles.header_btn} href="/Signup">
+                <a className={`${styles.header_btn} ${loggedIn ? styles.isLoggedIn : ""}`} href="/Signup">
                     <img className={styles.logo} src='/images/layout/header/signup-logo.svg'></img>
                     Đăng ký
                 </a>
